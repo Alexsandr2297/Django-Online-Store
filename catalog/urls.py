64ec4db7a8778bcd31e600a7ef553149e06c0c41
Alexsandr <warrn11@mail.ru>
2026-04-24 11:19:11 +0300

    feat: full implementation of the product catalog with image display

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from catalog.apps import CatalogConfig
+from catalog.views import catalog_list, catalog_detail
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', catalog_list, name='catalog_list'),
+    path('catalog/<int:pk>/', catalog_detail, name='catalog_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from gc import get_objects
 
-def index(request):
-    return HttpResponse("Добро пожаловать в интернет-магазин!")
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
+
+def catalog_list(request):
+    catalog = Product.objects.all()
+    context = {"catalogs": catalog}
+    return render(request, 'catalog_list.html', context)
+
+
+def catalog_detail(request, pk):
+    catalogs = get_object_or_404(Product, pk=pk)
+    context = {"catalog": catalogs}
+    return render(request, 'catalog_detail.html', context)
